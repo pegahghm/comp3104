f049b02ad7ca6d257711fbb6eae29a9b01b7d982
Pegah Ghods Mohammadi <101479024@georgebrown.ca>
2024-10-09 19:59:25 -0400

    added title

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101479024 || Pegah Ghods Mohammadi || DevOps</h5>
+      <h5>Developed By: 101479024 || Pegah Ghods Mohammadi || DevOps - 3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
